d0d43b60fed3b0ae96bebfe599b6257306dd8515
medhu07 <47105060+medhu07@users.noreply.github.com>
2025-06-02 08:53:07 +0000

    container config

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS CODE as newer version
+
+//alert("hello") we are using nodejs , not browser hence error 
+
+console.log("Medhansh")
+
+let name="Medhansh"
+let age=18
+let isLoggedin=false
+
+//number => 2^53
+//bigint
+//string=" "
+//boolean= true / false
+//null=>stand alone value (empty)
+//undefined => value not defined 
+//symbol =>unique
+
+
+//object
+
+console.log(typeof "Medhansh")
+
+
