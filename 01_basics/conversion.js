c6d60353b17c03acb09f18b9ac847a6611ea2546
medhu07 <47105060+medhu07@users.noreply.github.com>
2025-06-02 12:41:48 +0000

    container config

---
+++ 01_basics/conversion.js
@@ -0,0 +1,19 @@
+let score ="medhansh"
+console.log(typeof score);
+
+let valueInNumber = Number(score); // wway on conversion of string to number
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+ //"33"=> 33
+ //"33abc"=> NAN (not a number)
+ //true =1 ; false=0
+
+ let value =""
+ let booleanchange= Boolean(value)
+ console.log(typeof booleanchange)
+console.log(booleanchange)
+
+//1=> true , 0=> false
+//"" = false
+//"medhansh"=true
