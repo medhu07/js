1eb06aef9f6f62c2bfa3508a5abfff1d6c695987
medhu07 <47105060+medhu07@users.noreply.github.com>
2025-06-05 10:32:29 +0000

    container config

---
+++ 01_basics/object.js
@@ -0,0 +1,26 @@
+//object literal
+// carefully see syntax to describe an object in js 
+
+const Jsuser={
+    name:"Medhansh",
+    fullname:"Medhansh Mathur",
+    age :18,
+    location:"delhi",
+    email:"medhansh@gmail.com",
+    isLoggedIn:false
+
+
+
+}
+// use . to access elements of the object 
+console.log(Jsuser.email)
+Jsuser.email="medhuu@gmail.com"
+console.log(Jsuser.email)
+//Object.freeze(Jsuser)
+//Jsuser is freezed hence we cant change values now 
+
+Jsuser.greeting= function(){
+    console.log("Hello JS USER");
+}
+
+console.log(Jsuser.greeting) 
