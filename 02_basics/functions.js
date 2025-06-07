043ec783cc0b72b1f3e218c2a7d64423743c42fe
medhu07 <47105060+medhu07@users.noreply.github.com>
2025-06-07 08:12:18 +0000

    container config

--- 02_basics/functions.js
+++ 02_basics/functions.js
@@ -1,9 +1,20 @@
-function sayMyName(){
-    console.log("M")
-    console.log("e")
-    console.log("d")
-    console.log("h")
-    console.log("u")
+function addNummber(number1,number2){
+    number1=Number(number1)
+    number2=Number(number2)
+    console.log(number1 + number2)
+    return number1+number2
 }
 
-sayMyName()
+let vr=addNummber(3,"4")
+console.log(vr)
+
+function Login(username){
+    if(username==undefined){
+        console.log("pls enter a username")
+        return
+    }
+    return  `${username} just logged in`   //using backtags ` for string manipulation
+
+
+}
+console.log(Login("Medhansh"))
